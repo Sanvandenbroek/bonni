19de5c48f6c6516b5c1c3b13d9c2bc9376aebcc1
wolfez <wolfderechter@gmail.com>
2025-12-17 09:09:37 +0100

    use inline js script instead of separate file

--- index.js
+++
@@ -1,9 +0,0 @@
-const alleAfbeeldingen = document.querySelectorAll("img");
-const jaar = document.getElementById("jaar");
-
-alleAfbeeldingen.forEach(afbeelding => {
-  afbeelding.addEventListener('mouseover', function(){
-    jaar.innerText = afbeelding.getAttribute("data-jaar");
-  })
-})
-
